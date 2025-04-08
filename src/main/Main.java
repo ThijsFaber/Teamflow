2b6d012e1b4dead96552cfda80f46af523f55ae4
Thijs Faber <23107499@student.hhs.nl>
2025-04-08 16:11:11 +0200

    ALEXANDER CHECK OF DIT GOED IS

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,31 @@
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Scanner;
-
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+public class Main extends JFrame {
+    private CardLayout cardLayout;
+    private JPanel cardPanel;
     public static void main(String[] args) throws SQLException {
+        setTitle("Gebruikers Systeem");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+
+        // Create screen panels
+        loginScreen loginScreen = new loginScreen(this);
+        registerScreen registerScreen = new registerScreen(this);
+
+        // Add them to the layout
+        cardPanel.add(loginScreen, "login");
+        cardPanel.add(registerScreen, "register");
+
+        add(cardPanel);
+        cardLayout.show(cardPanel, "login");
+        
         Gebruiker gebruiker = new Gebruiker(1, "Alexander", "Sprint manager");
         Scrumelement epic1 = new Scrumelement(1, "Epic");
         ArrayList<Gebruiker> gebruikers = new ArrayList<>();
@@ -20,5 +42,14 @@ public class Main {
             String SQL = "SELECT * FROM scrumassistant.bericht LEFT JOIN scrumassistant.thread ON thread.ThreadID = bericht.Thread_ID LEFT JOIN scrumassistant.taken ON taken.TaakID = bericht.Taak_ID LEFT JOIN scrumassistant.epics ON epics.EpicID = bericht.Epic_ID LEFT JOIN scrumassistant.userstories ON userstories.UserStoryID = bericht.UserStory_ID ORDER BY bericht.datum desc";
             gebruiker.toonBerichten(SQL, gebruikers);
         }
+        
+        setVisible(true);
+    }
+    public void switchTo(String screenName) {
+        cardLayout.show(cardPanel, screenName);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Main::new);
     }
 }
